4d8b9f6bcb4efa0ce7d99df9e3c16ceae181fde9
Andrew Rossi <aroccorossi@gmail.com>
2021-04-25 13:13:03 -0400

    loading in the blog title

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,31 +1,23 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import { Helmet } from 'react-helmet';
-import Layout from '../components/Layout';
+// import { Helmet } from 'react-helmet';
+// import Layout from '../components/Layout';
 
 const BlogPostTemplate = ({ data }) => {
-  const siteData = data.allContentfulSiteSettings.edges[0].node;
-  // const post = get(this.props, 'data.contentfulBlogPost');
-  // const siteTitle = get(this.props, 'data.site.siteMetadata.title');
+  // const siteData = data.allContentfulSiteSettings.edges[0].node;
+  const post = data.contentfulBlogPost;
   
   return(
-    <Layout siteData={siteData}>Hello, World</Layout>
+    <div>{post.title}</div>
   )
 };
 
 export default BlogPostTemplate;
 
 export const pageQuery = graphql`
-  query PageQuery {
-    allContentfulSiteSettings {
-      edges {
-        node {
-          siteTitle
-          seoDescription {
-            seoDescription
-          }
-        }
-      }
+  query BlogPostBySlug($slug: String!) {
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
     }
   }
 `;
