3c61a1a2d55fa11738307f578bcaf8b901731453
Andrew Rossi <aroccorossi@gmail.com>
2021-04-25 12:11:08 -0400

    add lodash dependency, map posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,25 @@
-import * as React from "react";
-import { graphql } from "gatsby";
-import Layout from "../components/Layout";
-import Heading from "../components/heading/Heading";
+import React from 'react';
+import { graphql } from 'gatsby';
+import get from 'lodash/get';
+import Layout from '../components/Layout';
+import Heading from '../components/heading/Heading';
 
 const IndexPage = (props) => {
   const siteData = props.data.allContentfulSiteSettings.edges[0].node;
+  const posts = get(this, 'props.data.allContentfulBlogPost.edges')
 
   return (
     <Layout siteData={siteData}>
       <Heading>Hold on here...</Heading>
+      <div className="article-list">
+        {posts.map(({ node }) => {
+          return (
+            <li key={node.slug}>
+              hello
+            </li>
+          )
+        })}
+      </div>
     </Layout>
   );
 };
