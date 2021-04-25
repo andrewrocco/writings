a0c3321cfcdf68f1ed0917bf1b8fc6109993d503
Andrew Rossi <aroccorossi@gmail.com>
2021-04-25 14:22:44 -0400

    removing example code sample

--- src/components/blogPostBody/BlogPostBody.js
+++ src/components/blogPostBody/BlogPostBody.js
@@ -12,16 +12,6 @@ const options = {
   },
   renderNode: {
     [BLOCKS.PARAGRAPH]: (node, children) => <Paragraph>{children}</Paragraph>,
-    [BLOCKS.EMBEDDED_ASSET]: node => {
-      return (
-        <>
-          <h2>Embedded Asset</h2>
-          <pre>
-            <code>{JSON.stringify(node, null, 2)}</code>
-          </pre>
-        </>
-      )
-    },
   },
 };
 
