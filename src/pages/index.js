92b3cfac8153e389564241da95ecb71b15df430c
Andrew Rossi <aroccorossi@gmail.com>
2021-05-09 13:15:30 -0400

    adding some home page content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { graphql, Link } from 'gatsby';
 import Layout from '../components/Layout';
+import RichText from '../components/richText/RichText';
 import Heading from '../components/heading/Heading';
 
 const IndexPage = (props) => {
   const siteData = props.data.allContentfulSiteSettings.edges[0].node;
+  const homePageContent = props.data.allContentfulHomePage.edges[0].node;
   const posts = props.data.allContentfulBlogPost.edges;
 
   return (
     <Layout siteData={siteData}>
-      <Heading>Hold on here...</Heading>
+      <Heading>{homePageContent.title}</Heading>
+      <RichText document={homePageContent.body} />
       <div className="article-list">
         {posts.map(({ node }) => {
           return (
@@ -41,6 +44,9 @@ export const pageQuery = graphql`
       edges {
         node {
           title
+          body {
+            raw
+          }
         }
       }
     }
